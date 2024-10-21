bad32d000b623cef644fa5a5e62c84df3301a84d
Francisco Márquez <franciscomarquez2791@gmail.com>
2024-10-21 17:33:27 -0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -71,7 +71,7 @@ function App() {
         {listings.length > 0 ? (
           listings.map((listing, index) => (
             <div key={index} className="border p-2 mb-2">
-              <p><strong>Address: </strong>{listing.address}</p>
+              <p><strong>Address: how</strong>{listing.address}</p>
               <p><strong>URL:</strong> <a href={listing.url} target="_blank" rel="noopener noreferrer">{listing.url}</a></p>
               <p><strong>Price:</strong> {listing.price}</p>
               <p><strong>Expenses:</strong> {listing.expenses}</p>
